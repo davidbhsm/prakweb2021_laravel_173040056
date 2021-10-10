71cf069cd6f1670e1b0debafc9bffcba0c7996b3
davidbhsm <davidbhism@gmail.com>
2021-10-10 22:05:27 +0700

    menyelesaikan vidio 3 pertemuan 5

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "David Bhisma",
+        "email" => "davidbhism@gmail.com",
+        "image" => "david.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
